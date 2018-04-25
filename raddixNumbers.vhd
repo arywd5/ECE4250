b4779a3d6fc07ea1021263bb984cdd278feb4fd0
arywd5 <31825610+arywd5@users.noreply.github.com>
2018-04-25 10:31:25 -0500

    changed raddix to output and integer

--- raddixNumbers.vhd
+++ raddixNumbers.vhd
@@ -3,29 +3,29 @@ use IEEE.std_logic_1164.all;
 
 entity raddixNumbers is 
 	port(input: in std_logic_vector(2 downto 0);
-			output: out std_logic_vector(2 downto 0));
+			output: out integer range -2 to 2);
 end raddixNumbers;
 
 architecture behavior of raddixNumbers is 
 begin 
 	case input is
 		when "000" => 
-			output <= "000";
+			output := 0;
 		when "001" =>
-			output <= "001";
+			output := 1;
 		when "010" =>
-			output <= "001";
+			output := 1;
 		when "011" => 
-			output <= "010";
+			output := 2;
 		when "100" =>
-			output <= "110";
+			output := -2;
 		when "101" => 
-			output <= "111";
+			output := -1;
 		when "110" => 
-			output <= "111";
+			output := -1;
 		when "111" =>
-			output <= "000";
+			output := 0;
 		when others =>
-			output <= "000";
+			output := 0;
 	end case 
 end behavior;
