6e782fd2b095fa6655faf56aef22920e50d20a6d
arywd5 <31825610+arywd5@users.noreply.github.com>
2018-04-27 09:15:01 -0500

    changed to only one inout variable

--- negative.vhd
+++ negative.vhd
@@ -2,8 +2,7 @@ library IEEE;
 use std_logic_1164.all;
 
 entity negative is 
-	port(value: in std_logic_vector(15 downto 0);
-		neg: inout atd_logic_vector(15 downto 0);
+	port(value: inout std_logic_vector(15 downto 0));
 end negative;
 
 architecture behavior of negative is 
@@ -19,9 +18,9 @@ signal i: integer range 0 to 15 := 0;
 signal Cin, Cout: std_logic;
 begin
 		invertloop: for i in 0 to 15 loop
-			neg(i) <= not value(i);
+			value(i) <= not value(i);
 		end loop;
 			
 		Cin <= '0';	
-		ADD: eight_bit_adder port map(neg, one, Cin, neg, Cout);
+		ADD: eight_bit_adder port map(value, one, Cin, value, Cout);
 end behavior; 
