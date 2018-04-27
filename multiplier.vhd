586cbc4fb8414524547f5f397f34146ca13ff8f6
arywd5 <31825610+arywd5@users.noreply.github.com>
2018-04-27 09:37:10 -0500

    wrote multiplier code

--- multiplier.vhd
+++ multiplier.vhd
@@ -25,27 +25,33 @@ end component;
 --raddix numbers component 
 component raddixNumbers is 
 	port(input: in std_logic_vector(2 downto 0);
-		output: out integer range -2 to 2);
+	     	num: in std_logic_vector(7 downto 0);
+		output: out std_logic_vector(15 downto 0);
 end component;
 --negative component to take two's compliment of a number 
 component negative is 
-	port(value: in std_logic_vector(15 downto 0);
-		neg: inout std_logic_vector(15 downto 0));
+	port(value: inout std_logic_vector(15 downto 0));
 end compnent;
 --signals to be used in the multiplier
 signal shifted: std_logic_vector(8 downto 0);
 signal hold: integer range -2 to 2 := 0;
 signal j: integer range 0 to 15;
+signal W, X, Y, Z, hold1, hold2: std_logic_vector(15 downto 0);
+signal Cout, Cin: std_logic;	   
 
 begin
-	shifted <= B(7 downto 0) & "0";
-	product <= "0000000000000000";
-	
-	loop1: for j in 0 to 15 loop
-							
+	shifted <= B(7 downto 0) & "0";     
+	RN1: raddixNumbers port map(shifted(2 downto 0), A, W);
+	RN2: raddixNumbers port map(shifted(4 downto 2), A, X);
+	RN3: raddixNumbers port map(shifted(6 downto 4), A, Y);
+	RN4: raddixNumbers port map(shifted(8 downto 6), A, Z);
+	   
+	X <= X(13 downto 0) & "00";
+	Y <= Y(11 downto 0) & "0000";
+	Z <= Z(9 downto 0) & "000000";
 
-
-					
-	end loop;	
-
+	A1: eight_bit_adder port map(W, X, Cin, hold1, Cout);
+	A2: eight_bit_adder port map(Y, Z, Cin, hold2, Cout);
+	A3: eight_bit_adder port map(hold1, hold2, Cin, product, Cout);
+		
 end Behavior;
