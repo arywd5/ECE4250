e9b3b82ec47312fc43b4046128c0383fc33fb406
arywd5 <31825610+arywd5@users.noreply.github.com>
2018-04-27 09:17:01 -0500

    changed to return number to add

--- raddixNumbers.vhd
+++ raddixNumbers.vhd
@@ -3,29 +3,45 @@ use IEEE.std_logic_1164.all;
 
 entity raddixNumbers is 
 	port(input: in std_logic_vector(2 downto 0);
-			output: out integer range -2 to 2);
+	     num: in std_logic_vector(7 downto 0);
+			output: out std_logic_vector(15 downto 0);
 end raddixNumbers;
 
 architecture behavior of raddixNumbers is 
+component negative is 	     
+	port(value: inout std_logic_vector(15 downto 0));     
+end component; 	 
 begin 
+	 output <= "000000000000000";		--initialize output to all zeros 
 	case input is
+	     	--when our input is zero we dont need to add anything to the output 
 		when "000" => 
-			output := 0;
+			output <= "00000000000000";
+	     	--when our input is one our output stays the same as the input 
 		when "001" =>
-			output := 1;
+			output <= "00000000" & num(7 downto 0);
+	     	--same case as above 
 		when "010" =>
-			output := 1;
+			output <= "00000000" & num(7 downto 0);
+	     	--when 011 we need to multiple by two or shift left once
 		when "011" => 
-			output := 2;
+			output(8 downto 1) <= num(7 downto 0);
+	     	--shift left then take twos compliment 
 		when "100" =>
-			output := -2;
-		when "101" => 
-			output := -1;
-		when "110" => 
-			output := -1;
-		when "111" =>
-			output := 0;
+			output(8 downto 1) <= num(7 downto 0);
+	     		negative port map(output);		--is this correct function call?
+		--take twos compliment 
+	     	when "101" => 
+			output <= "00000000" & num(7 downto 0);
+	     		negative port map(output);
+	     	--same as case above 
+	     	when "110" => 
+			output <= "00000000" & num(7 downto 0);
+	     		negative port map(output);
+		--again output should be all zeros 
+	     	when "111" =>
+			output <= "00000000000000";
 		when others =>
-			output := 0;
+			output <= "00000000000000";
 	end case 
 end behavior;
